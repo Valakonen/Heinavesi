7689abc07e6b52b0a5d818d9b93b0a2105f22b1f
Markus Loukiainen <markusloukiainen1@gmail.com>
2020-03-24 15:56:11 +0200

    iframe test

--- ARevents.js
+++ ARevents.js
@@ -1,58 +1,9 @@
-let places;
-
-fetch("./places.json")
-  .then(function(resp)
-{
-  return resp.json();
-})
-.then(function(data)
-{
-  console.log(data);
-  places = data;
-})
-
-// Info screen toggle
-let infoActivated = false;
 let lastDetectedMarker;
-
-function toggleInfo()
-{
-
-  if (document.getElementById("info").style.display == "block")
-  {
-    document.getElementById("info").style.display = "none";
-  }
-  else
-  {
-    document.getElementById("info").style.display = "block";
-  }
-
-  if(infoActivated == false)
-  {
-    if(lastDetectedMarker != null)
-    {
-      infoActivated == true;
-      changePlaceInfo(lastDetectedMarker);
-    }
-  }
-  else
-  {
-    infoActivated == false;
-  }
-};
+var parentWindow = window.parent;
 
 function changeAudioSource(markerNumber)
 {
-  document.getElementById("audioPlayer").setAttribute('src', places[markerNumber].narrationLink);
-};
-
-// changes info text, image and video to match marker
-function changePlaceInfo(markerNumber)
-{
-  document.getElementById("placeTitle").innerHTML = places[markerNumber].name;
-  document.getElementById("placeDescription").innerHTML = places[markerNumber].description;
-  document.getElementById("infoVideo").src = places[markerNumber].videoLink;
-  document.getElementById("map").src = places[markerNumber].mapImage;
+  parentWindow.document.getElementById("audioPlayer").setAttribute('src', places[markerNumber].narrationLink);
 };
 
 window.addEventListener('camera-init', (data) => {
@@ -77,6 +28,7 @@ AFRAME.registerComponent('registerevents', {
         if (lastDetectedMarker != markerValue)
         {
           lastDetectedMarker = markerValue;
+          parentWindow.lastDetectedMarker = lastDetectedMarker;
           changeAudioSource(lastDetectedMarker);
         }
 
