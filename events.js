45832fc2968e3ed84416179bfd6746ea98ad30d9
Markus Loukiainen <markusloukiainen1@gmail.com>
2020-03-24 17:37:40 +0200

    iframe deleted

--- events.js
+++ events.js
@@ -1,4 +1,4 @@
-var places;
+let places;
 
 fetch("./places.json")
   .then(function(resp)
@@ -13,12 +13,7 @@ fetch("./places.json")
 
 // Info screen toggle
 let infoActivated = false;
-var lastDetectedMarker;
-
-function disablear()
-{
-  document.getElementById("arView").style.display = "none";
-};
+let lastDetectedMarker;
 
 function toggleInfo()
 {
@@ -59,3 +54,56 @@ function changePlaceInfo(markerNumber)
   document.getElementById("infoVideo").src = places[markerNumber].videoLink;
   document.getElementById("map").src = places[markerNumber].mapImage;
 };
+
+window.addEventListener('camera-init', (data) => {
+    console.log('camera-init', data);
+})
+
+window.addEventListener('camera-error', (error) => {
+    console.log('camera-error', error);
+})
+
+// Register events
+AFRAME.registerComponent('registerevents', {
+		init: function () {
+			var marker = this.el;
+
+			marker.addEventListener('markerFound', function() {
+				var markerValue = marker.getAttribute("value");
+
+        // update variable so info window can load right product
+
+        // if same marker detected again do not update audio source
+        if (lastDetectedMarker != markerValue)
+        {
+          lastDetectedMarker = markerValue;
+          changeAudioSource(lastDetectedMarker);
+        }
+
+				// TODO: Add your own code here to react to the marker being found.
+        if (markerValue == 0)
+        {
+
+        }
+        else if (markerValue == 1)
+        {
+
+        }
+        else if (markerValue == 2)
+        {
+
+        }
+
+        else if (markerValue == 3)
+        {
+
+        }
+
+			});
+
+			marker.addEventListener('markerLost', function() {
+				// TODO: Add your own code here to react to the marker being lost.
+
+			});
+		}
+  });
